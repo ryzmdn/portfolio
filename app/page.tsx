f2d5ff9d6b27c5a3c5da60bebfc5f7838de71355
ryzmdn <ryzmdn@gmail.com>
2026-06-17 22:56:58 +0700

    feat: added summary section about on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+const stats = [
+  { label: "Transactions every 24 hours", value: "44 million" },
+  { label: "Assets under holding", value: "$119 trillion" },
+  { label: "New users annually", value: "46,000" },
+];
+
 export default function Home() {
   return (
     <>
@@ -38,6 +44,42 @@ export default function Home() {
               recusandae ducimus obcaecati.
             </p>
             <Button>Email Me</Button>
+          </div>
+        </div>
+      </section>
+
+      <section className="mx-auto max-w-7xl w-full my-10 py-10 px-4 sm:px-6 lg:px-8">
+        <h2 className="text-4xl font-semibold tracking-tight text-pretty text-zinc-900 sm:text-5xl">
+          Our mission
+        </h2>
+        <div className="mt-6 flex flex-col gap-20 lg:flex-row">
+          <div className="lg:w-full lg:max-w-4xl lg:flex-auto">
+            <p className="text-xl/8 text-zinc-600">
+              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem.
+              At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at
+              vitae feugiat egestas ac. Diam nulla orci at in viverra
+              scelerisque eget. Eleifend egestas fringilla sapien.
+            </p>
+            <p className="mt-10 max-w-3xl text-base/7 text-zinc-700">
+              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
+              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
+              praesent donec est. Odio penatibus risus viverra tellus varius sit
+              neque erat velit. Faucibus commodo massa rhoncus, volutpat.
+              Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
+              sed turpis id.
+            </p>
+          </div>
+          <div className="lg:flex lg:flex-auto lg:justify-center">
+            <dl className="w-64 space-y-8 xl:w-80">
+              {stats.map((stat) => (
+                <div key={stat.label} className="flex flex-col-reverse gap-y-4">
+                  <dt className="text-base/7 text-zinc-600">{stat.label}</dt>
+                  <dd className="text-5xl font-semibold tracking-tight text-zinc-900">
+                    {stat.value}
+                  </dd>
+                </div>
+              ))}
+            </dl>
           </div>
         </div>
       </section>
